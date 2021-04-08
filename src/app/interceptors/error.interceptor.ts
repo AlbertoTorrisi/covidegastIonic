430cd4345d1378bf3bb84d17c259be9aab0d5e72
mirkotorrisi <mirko.torris92@gmail.com>
2021-04-08 19:45:30 +0200

    error alerts

--- src/app/interceptors/error.interceptor.ts
+++ src/app/interceptors/error.interceptor.ts
@@ -10,12 +10,15 @@ import { catchError } from 'rxjs/operators';
 
 import { AuthenticationService } from '../services/authentication.service';
 import { Router } from '@angular/router';
+import { AlertController } from '@ionic/angular';
 
 @Injectable()
 export class ErrorInterceptor implements HttpInterceptor {
   constructor(
     private authenticationService: AuthenticationService,
-    private router: Router
+    private router: Router,
+    private alertCtrl: AlertController,
+
   ) {}
 
   intercept(
@@ -24,16 +27,21 @@ export class ErrorInterceptor implements HttpInterceptor {
   ): Observable<HttpEvent<any>> {
     return next.handle(request).pipe(
       catchError((err) => {
-        console.log(err);
-        if (err.status === 401) {
-          this.authenticationService.logOut();
-          this.router.navigateByUrl('/login');
-        }
-        if (err.status === 404) {
-          this.router.navigateByUrl('/notFound');
-        }
-        const error = err.error.message || err.error.detail || err.statusText;
-        console.log(error);
+        console.log(err)
+        const error = err.error || err.error.detail || err.statusText;
+        const alert = this.alertCtrl.create({
+          header: 'ERROR',
+          message: error,
+          buttons: ['Close'],
+        }).then((alert)=> alert.present())
+          if (err.status === 401) {
+          
+            this.authenticationService.logOut();
+            this.router.navigateByUrl('/login');
+          }
+          if (err.status === 404) {
+            this.router.navigateByUrl('/notFound');
+          }
         return throwError(error);
       })
     );
