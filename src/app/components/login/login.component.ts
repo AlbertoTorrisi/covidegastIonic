1c8c88bb076d4a741abb6b536513d25948224e76
AlbertoTorrisi <alberto.torrisi5@gmail.com>
2021-04-08 22:00:13 +0200

    login form validators added

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -3,6 +3,7 @@ import { Component, OnInit } from '@angular/core';
 import { LoginService } from 'src/app/services/login.service';
 import { HttpResponse } from '@angular/common/http';
 import { Router } from '@angular/router';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -14,11 +15,25 @@ export class LoginComponent implements OnInit {
   password = '';
   message = '';
   errors: string[] = [];
+  loginForm: FormGroup;
+  blurUsername: boolean = false;
+  blurPassword: boolean = false;
   constructor(
     private service: LoginService,
     private router: Router,
-    private authService: AuthenticationService
-  ) {}
+    private authService: AuthenticationService,
+    private builder: FormBuilder
+  ) {
+    this.loginForm = builder.group({
+      username: [this.username, Validators.required],
+      password: [this.password, Validators.required],
+    });
+  }
+
+  blurInput(type) {
+    if (type === 'username') this.blurUsername = true;
+    if (type === 'password') this.blurPassword = true;
+  }
 
   ngOnInit(): void {}
   logIn = () => {
